308f625845c1c60f9137acbf20405fc3060bf61c
Artur Kutsev <kutsev11b@gmail.com>
2024-01-25 14:17:53 +0300

    99% complite

--- kidsbe/src/js/script.js
+++ kidsbe/src/js/script.js
@@ -12,3 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
         burgerButton.classList.toggle('burger-click');
     });
 });
+
+document.querySelector('.button_call').addEventListener('click', function() {
+    document.querySelector('.request').classList.remove('request_hide');
+    document.querySelector('.request').classList.add('request_active');
+  });
+
+document.querySelector('.request_close').addEventListener('click', function() {  
+    document.querySelector('.request').classList.remove('request_active');
+    document.querySelector('.request').classList.add('request_hide');
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const submitButton = document.querySelector('.request_submit_button');
+    submitButton.addEventListener('click', function() {
+      window.location.href = 'request.html';
+    });
+  });
